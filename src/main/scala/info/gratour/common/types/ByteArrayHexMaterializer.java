a36d7aed7b242da6531118b4ff105a7b244433bf
KwanKin Yau <alphax@vip.163.com>
2020-02-04 13:57:56 +0800

    v3 - change ByteArrayMaterializer to ByteArrayBase64Materializer, add ByteArrayHexMaterializer, StringUtils add hex decode function

---
+++ src/main/scala/info/gratour/common/types/ByteArrayHexMaterializer.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ *  Copyright (c) 2019, 2020 lucendar.com.
+ *  All rights reserved.
+ *
+ *  Contributors:
+ *     KwanKin Yau (alphax@vip.163.com) - initial API and implementation
+ *******************************************************************************/
+package info.gratour.common.types;
+
+import com.google.gson.*;
+import info.gratour.common.utils.StringUtils;
+
+import java.lang.reflect.Type;
+
+public class ByteArrayHexMaterializer implements JsonSerializer<byte[]>, JsonDeserializer<byte[]> {
+    @Override
+    public byte[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return StringUtils.hex(json.getAsString());
+    }
+
+    @Override
+    public JsonElement serialize(byte[] src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(StringUtils.hex(src));
+    }
+}
