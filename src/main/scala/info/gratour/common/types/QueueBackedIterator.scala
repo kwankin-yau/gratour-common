c1f8a0f6fd0da8879e7b9632030117bee4f558df
KwanKin Yau <alphax@vip.163.com>
2021-03-07 17:48:49 +0800

    1) QueueBackedIterator refactor 2) add Sink

--- src/main/scala/info/gratour/common/types/QueueBackedIterator.scala
+++ src/main/scala/info/gratour/common/types/QueueBackedIterator.scala
@@ -1,13 +1,32 @@
 package info.gratour.common.types
 
-import java.util.Iterator
+import java.util
 import java.util.concurrent.ArrayBlockingQueue
 import java.util.concurrent.atomic.AtomicBoolean
 
-class QueueBackedIterator[E](val queue: ArrayBlockingQueue[E], val eof: AtomicBoolean) extends Iterator[E] {
 
-  override def hasNext: Boolean = !eof.get()
+class QueueBackedIterator[E <: AnyRef](val capacity: Int) extends util.Iterator[E] with Sink[E] {
+
+  val queue: ArrayBlockingQueue[E] = new ArrayBlockingQueue[E](capacity)
+  val eofFlag: AtomicBoolean = new AtomicBoolean()
+
+  override def hasNext: Boolean = !eofFlag.get()
 
   override def next(): E =
     queue.take()
+
+  override def offer(e: E): Unit = queue.put(e)
+
+  override def eof(): Unit = eofFlag.set(true)
+
+  def toSink[SEND <: AnyRef](mapper: SEND => E): Sink[SEND] =
+    new Sink[SEND] {
+      override def offer(e: SEND): Unit = QueueBackedIterator.this.offer(mapper(e))
+
+      override def eof(): Unit = QueueBackedIterator.this.eof()
+    }
+}
+
+object QueueBackedIterator {
+  def apply[E <: AnyRef](capacity: Int): QueueBackedIterator[E] = new QueueBackedIterator[E](capacity)
 }
