643fe352397844c524240c8ac3310500536f4a9f
KwanKin Yau <alphax@vip.163.com>
2021-04-20 09:24:37 +0800

    1) fixed RandomString generated random string does not immutable

--- src/main/scala/info/gratour/common/utils/RandomString.java
+++ src/main/scala/info/gratour/common/utils/RandomString.java
@@ -11,6 +11,7 @@ public class RandomString {
      * Generate a random string.
      */
     public String nextString() {
+        char[] buf = new char[length];
         for (int idx = 0; idx < buf.length; ++idx)
             buf[idx] = symbols[random.nextInt(symbols.length)];
         return new String(buf);
@@ -27,15 +28,14 @@ public class RandomString {
     private final Random random;
 
     private final char[] symbols;
-
-    private final char[] buf;
+    private final int length;
 
     public RandomString(int length, Random random, String symbols) {
         if (length < 1) throw new IllegalArgumentException();
         if (symbols.length() < 2) throw new IllegalArgumentException();
         this.random = Objects.requireNonNull(random);
         this.symbols = symbols.toCharArray();
-        this.buf = new char[length];
+        this.length = length;
     }
 
     /**
